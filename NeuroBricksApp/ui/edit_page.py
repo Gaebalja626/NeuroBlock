5c67a10f8f6df35c2d15f69415e6be2e811306a2
Owen <parkyeonoh05@gmail.com>
2024-08-25 20:21:26 +0900

    qgraphicsview 분할 및 버튼 위치 재수정.

--- NeuroBricksApp/ui/edit_page.py
+++ NeuroBricksApp/ui/edit_page.py
@@ -1,6 +1,7 @@
 import sys
 from PyQt6 import QtWidgets, QtGui, QtCore
 
+
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
@@ -8,9 +9,14 @@ class Ui_MainWindow(object):
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
 
-        self.scene = QtWidgets.QGraphicsScene()
-        self.graphicsView = QtWidgets.QGraphicsView(self.scene, self.centralwidget)
-        self.graphicsView.setGeometry(QtCore.QRect(0, 0, 1920, 1080))
+        self.topScene = QtWidgets.QGraphicsScene()
+        self.topView = QtWidgets.QGraphicsView(self.topScene, self.centralwidget)
+        self.topView.setGeometry(QtCore.QRect(0, 0, 1920, 110))
+        self.topView.setAlignment(QtCore.Qt.AlignmentFlag.AlignLeft | QtCore.Qt.AlignmentFlag.AlignTop)
+
+        self.mainScene = QtWidgets.QGraphicsScene()
+        self.mainView = QtWidgets.QGraphicsView(self.mainScene, self.centralwidget)
+        self.mainView.setGeometry(QtCore.QRect(0, 110, 1920, 970))
 
         self.setupButtonsAndTabs()
 
@@ -19,10 +25,12 @@ class Ui_MainWindow(object):
         MainWindow.setStatusBar(self.statusbar)
 
     def setupButtonsAndTabs(self):
+        self.addButtons()
+
         tools_tab = QtWidgets.QTabWidget()
         edit_tab = QtWidgets.QTabWidget()
-        tools_tab.setGeometry(QtCore.QRect(0, 110, 241, 950))
-        edit_tab.setGeometry(QtCore.QRect(246, 110, 1670, 950))
+        tools_tab.setGeometry(QtCore.QRect(0, 0, 241, 950))
+        edit_tab.setGeometry(QtCore.QRect(246, 0, 1670, 950))
 
         tool_layer_widget = QtWidgets.QWidget()
         tool_data_widget = QtWidgets.QWidget()
@@ -31,15 +39,27 @@ class Ui_MainWindow(object):
         tools_tab.addTab(tool_layer_widget, "Layer")
         tools_tab.addTab(tool_data_widget, "Data")
         tools_tab.addTab(tool_custom_widget, "Custom")
-        edit_tab.addTab(QtWidgets.QWidget(), "Editor")
 
-        tools_proxy = self.scene.addWidget(tools_tab)
-        editor_proxy = self.scene.addWidget(edit_tab)
+        # Creating tabs for Preprocessing, DNN, and Pipeline
+        for category in ["Preprocessing", "DNN", "Pipeline"]:
+            tab_widget = QtWidgets.QTabWidget()
+            editor_widget = QtWidgets.QWidget()
+            code_widget = QtWidgets.QWidget()
 
-        tools_proxy.setPos(0, 110)
-        editor_proxy.setPos(246, 110)
+            # Setting layouts and adding graphics views
+            editor_layout = QtWidgets.QVBoxLayout(editor_widget)
+            code_layout = QtWidgets.QVBoxLayout(code_widget)
 
-        self.addButtons()
+            editor_layout.addWidget(QtWidgets.QGraphicsView())
+            code_layout.addWidget(QtWidgets.QGraphicsView())
+
+            tab_widget.addTab(editor_widget, "Editor")
+            tab_widget.addTab(code_widget, "Code")
+
+            edit_tab.addTab(tab_widget, category)
+
+        self.mainScene.addWidget(tools_tab)
+        self.mainScene.addWidget(edit_tab)
 
     def addButtons(self):
         icons = [
@@ -48,14 +68,16 @@ class Ui_MainWindow(object):
             "icons8-sort-48.png", "icons8-pipeline-32.png"
         ]
         captions = ["Load", "Save", "Export", "Preprocess", "DNN", "Pipeline", "Sort", "Absorb"]
-        positions = [(10, 10), (110, 10), (210, 10), (310, 10), (410, 10), (510, 10), (610, 10), (710, 10)]
-
-        for icon, caption, pos in zip(icons, captions, positions):
+        x = 0
+        y = 10
+        for icon, caption in zip(icons, captions):
             button = QtWidgets.QPushButton(caption)
             button.setIcon(QtGui.QIcon(f"./image/{icon}"))
             button.setIconSize(QtCore.QSize(64, 64))
-            button_proxy = self.scene.addWidget(button)
-            button_proxy.setPos(*pos)
+            buttonProxy = self.topScene.addWidget(button)
+            buttonProxy.setPos(x, y)
+            x += 110
+
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
